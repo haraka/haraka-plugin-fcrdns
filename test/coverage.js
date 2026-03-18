a2df9a18bc8f8677973d820367707a136177796c
Matt Simerson <matt@tnpi.net>
2026-03-17 21:28:27 -0700

    - esnext
    - test: arrow functions
    - deps: bump all versions to latest
    - style: refactored most callbacks to async
    - test: refactored tests into tables
    - test: added a lot of them
    - test: working test coverage
    - ci: updated shared workflows

---
+++ test/coverage.js
@@ -0,0 +1,78 @@
+const assert = require('node:assert/strict')
+const dns = require('node:dns')
+const { beforeEach, describe, it } = require('node:test')
+
+const constants = require('haraka-constants')
+const fixtures = require('haraka-test-fixtures')
+
+describe('coverage hooks', () => {
+  beforeEach(async () => {
+    this.plugin = new fixtures.plugin('fcrdns')
+    this.plugin.register()
+    this.connection = new fixtures.connection.createConnection()
+    this.connection.init_transaction()
+    await new Promise((resolve) => {
+      this.plugin.initialize_fcrdns(resolve, this.connection)
+    })
+  })
+
+  it('initialize_fcrdns seeds deterministic result keys', () => {
+    this.plugin.initialize_fcrdns(() => {}, this.connection)
+
+    const res = this.connection.results.get('fcrdns')
+    assert.ok(Array.isArray(res.fcrdns))
+    assert.equal(res.has_rdns, false)
+    assert.equal(typeof res.ptr_name_to_ip, 'object')
+  })
+
+  it('handle_ptr_error stores err for unknown DNS failures', () => {
+    const err = new Error('boom')
+    err.code = 'ETIMEOUT'
+
+    this.plugin.handle_ptr_error(this.connection, err, (rc, msg) => {
+      assert.equal(rc, undefined)
+      assert.equal(msg, undefined)
+    })
+
+    const res = this.connection.results.get('fcrdns')
+    if (Array.isArray(res.err)) {
+      assert.ok(res.err.includes('ETIMEOUT'))
+      return
+    }
+    assert.equal(res.err, 'ETIMEOUT')
+  })
+
+  it('handle_ptr_error denies no-rdns when configured', () => {
+    this.plugin.cfg.reject.no_rdns = true
+
+    const err = new Error('missing')
+    err.code = dns.NOTFOUND
+
+    this.plugin.handle_ptr_error(this.connection, err, (rc) => {
+      assert.equal(rc, constants.DENY)
+    })
+  })
+
+  it('save_auth_results maps err to iprev temperror', () => {
+    this.connection.results.add(this.plugin, { has_rdns: true })
+    this.connection.results.add(this.plugin, { err: 'ETIMEOUT' })
+
+    const result = this.plugin.save_auth_results(this.connection)
+
+    assert.equal(result, false)
+  })
+
+  it('ptr_compare exact match passes', () => {
+    this.connection.remote.ip = '10.1.2.3'
+
+    const matched = this.plugin.ptr_compare(
+      ['10.1.2.3'],
+      this.connection,
+      'mx.example.test',
+    )
+
+    assert.equal(matched, true)
+    const res = this.connection.results.get('fcrdns')
+    assert.ok(res.fcrdns.includes('mx.example.test'))
+  })
+})
--- test/index.js
+++ test/index.js
@@ -1,171 +1,103 @@
-// node.js built-ins
-const assert = require('node:assert')
+const assert = require('node:assert/strict')
 const dns = require('node:dns')
+const { beforeEach, describe, it } = require('node:test')
 
-// npm modules
+const constants = require('haraka-constants')
 const fixtures = require('haraka-test-fixtures')
 
-beforeEach(function (done) {
+beforeEach(async () => {
   this.plugin = new fixtures.plugin('fcrdns')
   this.plugin.register()
   this.connection = new fixtures.connection.createConnection()
   this.connection.init_transaction()
-  this.plugin.initialize_fcrdns(done, this.connection)
+  await new Promise((resolve) => {
+    this.plugin.initialize_fcrdns(resolve, this.connection)
+  })
 })
 
-describe('fcrdns', function () {
-  it('loads', function () {
+describe('fcrdns', () => {
+  it('loads', () => {
     assert.ok(this.plugin)
   })
 })
 
-describe('load_fcrdns_ini', function () {
-  it('loads fcrdns.ini from config/fcrdns.ini', function () {
+describe('load_fcrdns_ini', () => {
+  it('loads fcrdns.ini from config/fcrdns.ini', () => {
     this.plugin.load_fcrdns_ini()
     assert.ok(this.plugin.cfg)
   })
 
-  it('initializes enabled boolean', function () {
+  it('initializes enabled boolean', () => {
     this.plugin.load_fcrdns_ini()
     assert.equal(this.plugin.cfg.reject.no_rdns, false, this.plugin.cfg)
   })
 })
 
-describe('handle_ptr_error', function () {
-  it('ENOTFOUND reject.no_rdns=0', function (done) {
-    const err = new Error('test error')
+describe('handle_ptr_error', () => {
+  for (const [code, reject, expected] of [
+    [dns.NOTFOUND, false, undefined],
+    [dns.NOTFOUND, true, constants.DENY],
+    ['fake', false, undefined],
+    ['fake', true, constants.DENYSOFT],
+  ]) {
+    it(`${code} reject=${reject}`, () => {
+      const err = new Error('test')
+      err.code = code
+      this.plugin.cfg.reject.no_rdns = reject
+      this.plugin.handle_ptr_error(this.connection, err, (rc) =>
+        assert.equal(rc, expected),
+      )
+    })
+  }
+
+  it('whitelisted skips reject', () => {
+    const err = new Error('test')
     err.code = dns.NOTFOUND
-    this.plugin.handle_ptr_error(this.connection, err, function () {
-      assert.equal(undefined, arguments[0])
-      done()
-    })
-  })
-
-  it('ENOTFOUND reject.no_rdns=1', function (done) {
-    const err = new Error('test error')
-    err.code = dns.NOTFOUND
-    this.plugin.cfg.reject.no_rdns = 1
-    this.plugin.handle_ptr_error(this.connection, err, function () {
-      assert.equal(DENY, arguments[0])
-      done()
-    })
-  })
-
-  it('dns.NOTFOUND reject.no_rdns=0', function (done) {
-    const err = new Error('test error')
-    err.code = dns.NOTFOUND
-    this.plugin.cfg.reject.no_rdns = 0
-    this.plugin.handle_ptr_error(this.connection, err, function () {
-      assert.equal(undefined, arguments[0])
-    })
-    done()
-  })
-
-  it('dns.NOTFOUND reject.no_rdns=1', function (done) {
-    const err = new Error('test error')
-    err.code = dns.NOTFOUND
-    this.plugin.cfg.reject.no_rdns = 1
-    this.plugin.handle_ptr_error(this.connection, err, function () {
-      assert.equal(DENY, arguments[0])
-      done()
-    })
-  })
-
-  it('dns.FAKE reject.no_rdns=0', function (done) {
-    const err = new Error('test error')
-    err.code = 'fake'
-    this.plugin.cfg.reject.no_rdns = 0
-    this.plugin.handle_ptr_error(this.connection, err, function () {
-      assert.equal(undefined, arguments[0])
-    })
-    done()
-  })
-
-  it('dns.FAKE reject.no_rdns=1', function (done) {
-    const err = new Error('test error')
-    err.code = 'fake'
-    this.plugin.cfg.reject.no_rdns = 1
-    this.plugin.handle_ptr_error(this.connection, err, function () {
-      assert.equal(DENYSOFT, arguments[0])
-      done()
-    })
+    this.plugin.cfg.reject.no_rdns = true
+    this.connection.notes.rdns_access = 'white'
+    this.plugin.handle_ptr_error(this.connection, err, (rc) =>
+      assert.equal(rc, undefined),
+    )
   })
 })
 
-describe('is_generic_rdns', function () {
-  it('mail.theartfarm.com', function (done) {
-    this.connection.remote.ip = '66.128.51.165'
-    assert.equal(
-      false,
-      this.plugin.is_generic_rdns(this.connection, 'mail.theartfarm.com'),
-    )
-    done()
-  })
-
-  it('dsl-188-34-255-136.asretelecom.net', function (done) {
-    this.connection.remote.ip = '188.34.255.136'
-    assert.ok(
-      this.plugin.is_generic_rdns(
-        this.connection,
-        'dsl-188-34-255-136.asretelecom.net',
-      ),
-    )
-    done()
-  })
-
-  it('c-76-121-96-159.hsd1.wa.comcast.net', function (done) {
-    this.connection.remote.ip = '76.121.96.159'
-    assert.ok(
-      this.plugin.is_generic_rdns(
-        this.connection,
-        'c-76-121-96-159.hsd1.wa.comcast.net',
-      ),
-    )
-    done()
-  })
-
-  it('c-76-121-96-159.business.wa.comcast.net', function (done) {
-    this.connection.remote.ip = '76.121.96.159'
-    assert.equal(
-      false,
-      this.plugin.is_generic_rdns(
-        this.connection,
-        'c-76-121-96-159.business.wa.comcast.net',
-      ),
-    )
-    done()
-  })
-  it('null', function (done) {
-    this.connection.remote.ip = '192.168.1.1'
-    assert.equal(false, this.plugin.is_generic_rdns(this.connection, null))
-    done()
-  })
-  it('tld, com', function (done) {
-    this.connection.remote.ip = '192.168.1.1'
-    assert.equal(false, this.plugin.is_generic_rdns(this.connection, 'com'))
-    done()
-  })
-  it('empty string', function (done) {
-    this.connection.remote.ip = '192.168.1.1'
-    assert.equal(false, this.plugin.is_generic_rdns(this.connection, ''))
-    done()
-  })
+describe('is_generic_rdns', () => {
+  for (const [ip, host, expected] of [
+    ['66.128.51.165', 'mail.theartfarm.com', false],
+    ['188.34.255.136', 'dsl-188-34-255-136.asretelecom.net', true],
+    ['76.121.96.159', 'c-76-121-96-159.hsd1.wa.comcast.net', true],
+    ['76.121.96.159', 'c-76-121-96-159.business.wa.comcast.net', false],
+    ['192.168.1.1', null, false],
+    ['192.168.1.1', 'com', false],
+    ['192.168.1.1', '', false],
+    ['192.168.1.1', '192.168.1.1', false],
+  ]) {
+    it(String(host), () => {
+      this.connection.remote.ip = ip
+      assert.equal(expected, this.plugin.is_generic_rdns(this.connection, host))
+    })
+  }
 })
 
-describe('save_auth_results', function () {
-  it('fcrdns fail', function () {
+describe('save_auth_results', () => {
+  it('fcrdns fail', () => {
     this.connection.results.add(this.plugin, { pass: 'fcrdns' })
     assert.equal(false, this.plugin.save_auth_results(this.connection))
   })
 
-  it('fcrdns pass', function () {
+  it('fcrdns pass', () => {
     this.connection.results.push(this.plugin, { fcrdns: 'example.com' })
     assert.equal(true, this.plugin.save_auth_results(this.connection))
   })
+
+  it('iprev=fail when rdns but no fcrdns match', () => {
+    this.connection.results.add(this.plugin, { has_rdns: true })
+    assert.equal(false, this.plugin.save_auth_results(this.connection))
+  })
 })
 
-describe('ptr_compare', function () {
-  it('fail', function () {
+describe('ptr_compare', () => {
+  it('fail', () => {
     this.connection.remote.ip = '10.1.1.1'
     const iplist = ['10.0.1.1']
     assert.equal(
@@ -174,7 +106,7 @@ describe('ptr_compare', function () {
     )
   })
 
-  it('pass exact', function () {
+  it('pass exact', () => {
     this.connection.remote.ip = '10.1.1.1'
     const iplist = ['10.1.1.1']
     assert.equal(
@@ -183,7 +115,7 @@ describe('ptr_compare', function () {
     )
   })
 
-  it('pass net', function () {
+  it('pass net', () => {
     this.connection.remote.ip = '10.1.1.1'
     const iplist = ['10.1.1.2']
     assert.equal(
@@ -193,118 +125,150 @@ describe('ptr_compare', function () {
   })
 })
 
-describe('check_fcrdns', function () {
-  it('fail, tolerate', function (done) {
+describe('check_fcrdns', () => {
+  it('fail, tolerate', () => {
     this.connection.remote.ip = '10.1.1.1'
     const results = { 'foo.example.com': [] }
-    this.plugin.check_fcrdns(this.connection, results, function (rc, msg) {
+    this.plugin.check_fcrdns(this.connection, results, (rc, msg) => {
       assert.equal(rc, undefined)
       assert.equal(msg, undefined)
-      done()
     })
   })
 
-  it('null host', function (done) {
-    // this result was experienced "in the wild"
+  it('null host', () => {
     this.connection.remote.ip = '10.1.1.1'
     const results = { 'foo.example.com': ['', null] }
-    this.plugin.check_fcrdns(this.connection, results, function (rc, msg) {
+    this.plugin.check_fcrdns(this.connection, results, (rc, msg) => {
       assert.equal(rc, undefined)
       assert.equal(msg, undefined)
-      done()
     })
+  })
+
+  it('denies generic_rdns when configured', () => {
+    this.connection.remote.ip = '188.34.255.136'
+    this.plugin.cfg.reject.generic_rdns = true
+    const results = { 'dsl-188-34-255-136.asretelecom.net': [] }
+    this.plugin.check_fcrdns(this.connection, results, (rc, msg) => {
+      assert.equal(rc, constants.DENY)
+      assert.ok(/generic rDNS/.test(msg))
+    })
+  })
+
+  it('denies no_fcrdns when configured', () => {
+    this.connection.remote.ip = '10.1.1.1'
+    this.plugin.cfg.reject.no_fcrdns = true
+    const results = { 'mail.example.com': ['10.2.2.3'] }
+    this.plugin.check_fcrdns(this.connection, results, (rc, msg) => {
+      assert.equal(rc, constants.DENY)
+      assert.equal(msg, 'Sorry, no FCrDNS match found')
+    })
+  })
+
+  it('detects ptr_multidomain', () => {
+    this.connection.remote.ip = '10.1.1.1'
+    const results = {
+      'mail.example.com': ['10.1.1.1'],
+      'smtp.other.net': ['10.1.1.1'],
+    }
+    this.plugin.check_fcrdns(this.connection, results, () => {})
+    assert.equal(this.connection.results.store.fcrdns.ptr_multidomain, true)
   })
 })
 
-describe('resolve_ptr_names', function () {
-  this.timeout(5000)
+describe('resolve_ptr_names', () => {
+  const validCases = ['mail.theartfarm.com', 'smtp.gmail.com']
 
-  const validCases = {
-    'mail.theartfarm.com': [],
-    'smtp.gmail.com': [],
-  }
-
-  for (const c in validCases) {
-    it(`gets IPs for ${c}`, function (done) {
+  for (const c of validCases) {
+    it(`gets IPs for ${c}`, { timeout: 5000 }, async () => {
       const ptr_names = [c]
-      this.plugin.resolve_ptr_names(ptr_names, this.connection, () => {
-        // console.log(this.connection.results.store.fcrdns)
-        assert.ok(this.connection.results.store.fcrdns.ptr_name_to_ip[c].length)
-        assert.equal(
-          this.connection.results.store.fcrdns.ptr_name_has_ips,
-          true,
-        )
-        done()
+      await new Promise((resolve) => {
+        this.plugin.resolve_ptr_names(ptr_names, this.connection, () => {
+          assert.ok(
+            this.connection.results.store.fcrdns.ptr_name_to_ip[c].length,
+          )
+          assert.equal(
+            this.connection.results.store.fcrdns.ptr_name_has_ips,
+            true,
+          )
+          resolve()
+        })
       })
     })
   }
 
-  it('ignores invalid host names', function (done) {
+  it('ignores invalid host names', async () => {
     const ptr_names = ['mail.invalid']
-    this.plugin.resolve_ptr_names(ptr_names, this.connection, () => {
-      // console.log(this.connection.results.store.fcrdns)
-      assert.ok(this.connection.results.store.fcrdns.other_ips.length === 0)
-      done()
+    await new Promise((resolve) => {
+      this.plugin.resolve_ptr_names(ptr_names, this.connection, () => {
+        assert.equal(this.connection.results.store.fcrdns.other_ips.length, 0)
+        resolve()
+      })
     })
   })
 })
 
-describe('do_dns_lookups', function () {
+describe('do_dns_lookups', () => {
   const testIps = {
     '8.8.4.4': 'dns.google',
     '2001:4860:4860::8844': 'dns.google',
     '4.2.2.2': 'level3.net',
-    // '208.67.222.222': 'opendns.com',
     '1.1.1.1': 'one.one',
   }
 
   for (const ip of Object.keys(testIps)) {
-    it(`looks up ${ip}`, function (done) {
-      this.timeout(5000)
-
+    it(`looks up ${ip}`, { timeout: 5000 }, async () => {
       this.connection.remote.ip = ip
 
-      this.plugin.do_dns_lookups((rc, msg) => {
-        const res = this.connection.results.get('fcrdns')
-        assert.ok(new RegExp(testIps[ip]).test(res.fcrdns[0]))
-        // console.log(res);
-        assert.equal(rc, undefined)
-        assert.equal(msg, undefined)
-        done()
-      }, this.connection)
+      await new Promise((resolve) => {
+        this.plugin.do_dns_lookups((rc, msg) => {
+          const res = this.connection.results.get('fcrdns')
+          assert.ok(new RegExp(testIps[ip]).test(res.fcrdns[0]))
+          assert.equal(rc, undefined)
+          assert.equal(msg, undefined)
+          resolve()
+        }, this.connection)
+      })
     })
   }
 })
 
-describe('add_message_headers', function () {
-  it('removes message headers', function (done) {
+describe('add_message_headers', () => {
+  it('handles missing fcrdns results', () => {
+    const conn = fixtures.connection.createConnection()
+    conn.init_transaction()
+    this.plugin.add_message_headers((rc) => assert.equal(rc, undefined), conn)
+  })
+
+  it('removes message headers', () => {
     this.connection.transaction.add_header('X-Haraka-FCrDNS', 'example.com')
     this.connection.transaction.add_header('X-Haraka-rDNS-OtherIPs', '1.2.3.4')
 
-      this.plugin.add_message_headers((rc, msg) => {
-        const fcrdns = this.connection.transaction.header.get('X-Haraka-FCrDNS')
-        const other_ips = this.connection.transaction.header.get('X-Haraka-rDNS-OtherIPs')
-        assert.equal(fcrdns, '')
-        assert.equal(other_ips, '')
-        assert.equal(rc, undefined)
-        assert.equal(msg, undefined)
-        done()
-      }, this.connection)
+    this.plugin.add_message_headers((rc, msg) => {
+      const fcrdns = this.connection.transaction.header.get('X-Haraka-FCrDNS')
+      const other_ips = this.connection.transaction.header.get(
+        'X-Haraka-rDNS-OtherIPs',
+      )
+      assert.equal(fcrdns, '')
+      assert.equal(other_ips, '')
+      assert.equal(rc, undefined)
+      assert.equal(msg, undefined)
+    }, this.connection)
   })
 
-  it('adds message headers', function (done) {
-      this.connection.results.push('fcrdns', {fcrdns: 'mail.example.com'})
-      this.connection.results.push('fcrdns', {fcrdns: 'example.com'})
-      this.connection.results.push('fcrdns', {other_ips: '1.2.3.4'})
+  it('adds message headers', () => {
+    this.connection.results.push('fcrdns', { fcrdns: 'mail.example.com' })
+    this.connection.results.push('fcrdns', { fcrdns: 'example.com' })
+    this.connection.results.push('fcrdns', { other_ips: '1.2.3.4' })
 
-      this.plugin.add_message_headers((rc, msg) => {
-        const fcrdns = this.connection.transaction.header.get('X-Haraka-FCrDNS')
-        const other_ips = this.connection.transaction.header.get('X-Haraka-rDNS-OtherIPs')
-        assert.equal(fcrdns, 'mail.example.com example.com')
-        assert.equal(other_ips, '1.2.3.4')
-        assert.equal(rc, undefined)
-        assert.equal(msg, undefined)
-        done()
-      }, this.connection)
+    this.plugin.add_message_headers((rc, msg) => {
+      const fcrdns = this.connection.transaction.header.get('X-Haraka-FCrDNS')
+      const other_ips = this.connection.transaction.header.get(
+        'X-Haraka-rDNS-OtherIPs',
+      )
+      assert.equal(fcrdns, 'mail.example.com example.com')
+      assert.equal(other_ips, '1.2.3.4')
+      assert.equal(rc, undefined)
+      assert.equal(msg, undefined)
+    }, this.connection)
   })
 })
